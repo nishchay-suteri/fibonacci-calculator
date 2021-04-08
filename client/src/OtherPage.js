a2765e0ab777091bc19edd7542e87828eff585e6
nishchay271095 <nishchay271095@gmail.com>
2021-04-08 08:40:45 +0530

    fix: Resolve compilation warning

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom"
 
-export default () => {
+const otherPageComponent = () => {
     return (
         <div>
             I'm some other page!
@@ -9,3 +9,5 @@ export default () => {
         </div>
     );
 }
+
+export default otherPageComponent;
